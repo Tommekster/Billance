984379e540d9cbd06461ced0436d595be562b423
Tomas Zikmund <tommekster@gmail.com>
2017-02-17 19:20:22 +0100

    findFlat

--- src/billance/Flat.java
+++ src/billance/Flat.java
@@ -5,18 +5,61 @@
  */
 package billance;
 
+import java.lang.reflect.Field;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Acer
  */
 public class Flat {
+    private int id;
+    public int waterId;
+    public int heatId;
+    public int electricityId;
+    public int surface;
+    
     public static String [] getFlats() throws ClassNotFoundException, SQLException {
         /*Integer [] flats = Database.getInstance().getFlats();
         String [] flatNames = new String [flats.length];
         for(int i = 0; i < flats.length; i++) flatNames[i] = Integer.toString(flats[i].intValue());
         return flatNames;*/
         return Database.getInstance().getFlats();
+    }
+    
+    private Flat() {}
+
+    static Flat findFlat(int flatId) {
+        try {
+            ResultSet rs = Database.getInstance().findFlat(flatId);
+            Flat f = new Flat();
+            f.id = flatId;
+            Field [] fields = Flat.class.getFields();
+            for(Field field : fields){
+                rs.getInt(field.getName());
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Tariff.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public String getTM() {
+        return "cm"+heatId;
+    }
+
+    public String getWM() {
+        return "wm"+waterId;
+    }
+
+    public String getVT() {
+        return "vt"+electricityId;
+    }
+
+    public String getNT() {
+        return "nt"+electricityId;
     }
 }
